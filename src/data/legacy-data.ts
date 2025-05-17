bc0f1f41031e635af6a5fcd3b114a4988d8f10b5
Dinush Chathurya <dinushchathurya21@gmail.com>
2025-05-17 16:25:02 +0100

    :bug: Remove duplicate data from legacy-data.ts

--- src/data/legacy-data.ts
+++ src/data/legacy-data.ts
@@ -6,7 +6,7 @@ import { LegacyUniversityData } from '../types';
  */
 export const legacyData: LegacyUniversityData = {
   "University of Colombo": {
-      "Faculty of Arts": [ 
+      "Faculty of Arts": [
         "Bachelor of Arts (General)",
         "Bachelor of Arts (Special) in Demography",
         "Bachelor of Arts (Special) in Economics",
@@ -27,11 +27,10 @@ export const legacyData: LegacyUniversityData = {
       "Faculty of Management and Finance": [
         "Bachelor of Business Administration (Special) in Accounting",
         "Bachelor of Business Administration (Special) in Accounting and Finance",
-        "Bachelor of Business Administration (Special) in Business Economics", 
+        "Bachelor of Business Administration (Special) in Business Economics",
         "Bachelor of Business Administration (Special) in Entrepreneurship",
         "Bachelor of Business Administration (Special) in Finance",
         "Bachelor of Business Administration (Special) in Hospitality and Leisure Management",
-        "Bachelor of Business Administration (Special) in Human Resource Management",
         "Bachelor of Business Administration (Special) in Human Resource Management",
         "Bachelor of Business Administration (Special) in International Business",
         "Bachelor of Business Administration (Special) in Management and Organizational Studies",
@@ -42,13 +41,10 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Medicine and Bachelor of Surgery",
         "Bachelor of Pharmacy in Pharmacy",
         "Bachelor of Science in Medical Laboratory Sciences",
-        "Bachelor of Science in Medical Laboratory Sciences",
-        "Bachelor of Science in Nursing",
         "Bachelor of Science in Nursing",
         "Bachelor of Science in Pharmacy",
         "Bachelor of Science in Physiotherapy",
         "Bachelor of Science in Speech and Hearing Sciences",
-        "MBBS",
         "MBBS"
       ],
       "Faculty of Science": [
@@ -57,7 +53,6 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Science",
         "Bachelor of Science (Biological Sciences) (General)",
         "Bachelor of Science (General)",
-        "Bachelor of Science (General)",
         "Bachelor of Science (General) in Environmental Conservation and Management",
         "Bachelor of Science (General) in Industrial Statistics and Mathematical Finance",
         "Bachelor of Science (General) in Management and Information Technology",
@@ -65,10 +60,7 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Science (Special) Software Engineering",
         "Bachelor of Science (Special) in Bioinfomatics",
         "Bachelor of Science (Special) in Botany",
-        "Bachelor of Science (Special) in Botany",
         "Bachelor of Science (Special) in Chemical Biology",
-        "Bachelor of Science (Special) in Chemistry",
-        "Bachelor of Science (Special) in Chemistry",
         "Bachelor of Science (Special) in Chemistry",
         "Bachelor of Science (Special) in Computational Physics",
         "Bachelor of Science (Special) in Computer Science",
@@ -81,8 +73,6 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Science (Special) in Information Technology",
         "Bachelor of Science (Special) in Mathematical Physics",
         "Bachelor of Science (Special) in Mathematics",
-        "Bachelor of Science (Special) in Mathematics",
-        "Bachelor of Science (Special) in Mathematics",
         "Bachelor of Science (Special) in Mathematics and Statistics with Computer Science",
         "Bachelor of Science (Special) in Microbiology",
         "Bachelor of Science (Special) in Molecular Biology and Biochemistry",
@@ -90,21 +80,12 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Science (Special) in Nuclear Medical Science",
         "Bachelor of Science (Special) in Pharmacy",
         "Bachelor of Science (Special) in Physics",
-        "Bachelor of Science (Special) in Physics",
         "Bachelor of Science (Special) in Plant Biotechnology",
         "Bachelor of Science (Special) in Plant Science",
         "Bachelor of Science (Special) in Statistics",
-        "Bachelor of Science (Special) in Statistics",
         "Bachelor of Science (Special) in Statistics with Computer Science",
         "Bachelor of Science (Special) in Zoology",
-        "Bachelor of Science (Special) in Zoology",
-        "Bachelor of Science (Special) in Zoology",
-        "Bachelor of Science in Biochemistry and Molecular Biology (General)",
-        "Degree of Bachelor of Science Special in Botany",
-        "Degree of Bachelor of Science Special in Chemistry",
-        "Degree of Bachelor of Science Special in Computer Science and Mathematics",
-        "Degree of Bachelor of Science Special in Physics",
-        "Degree of Bachelor of Science Special in Zoology"
+        "Bachelor of Science in Biochemistry and Molecular Biology (General)"
       ]
     },
     "University of Peradeniya": {
@@ -126,12 +107,11 @@ export const legacyData: LegacyUniversityData = {
       ],
       "Faculty of Arts": [
         "Bachelor of Arts (General)",
-        "Bachelor of Arts (General)",
         "Bachelor of Arts (Special)",
         "Bachelor of Arts (Special) in Arabics",
         "Bachelor of Arts (Special) in Archaeology",
         "Bachelor of Arts (Special) in Buddhist Studies",
-        "Bachelor of Arts (Special) in Economics", 
+        "Bachelor of Arts (Special) in Economics",
         "Bachelor of Arts (Special) in English",
         "Bachelor of Arts (Special) in Fine Arts",
         "Bachelor of Arts (Special) in Geography",
@@ -154,7 +134,6 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Fine Arts (Art and Design)",
         "Bachelor of Fine Arts (Dance)",
         "Bachelor of Fine Arts (Music)",
-        "Bachelor of Law",
         "Bachelor of Law"
       ],
       "Faculty of Dental Sciences": [
@@ -198,30 +177,66 @@ export const legacyData: LegacyUniversityData = {
     },
     "University of Sri Jayewardenepura": {
       "Faculty of Applied Sciences": [
+        "B.Sc. (Applied Mathematics and Computing)",
+        "B.Sc. (Environmental Science)",
+        "B.Sc. (Special in Applied Mathematics and Computing)",
+        "B.Sc. (Special in Environmental Science)",
+        "B.Sc. (Special) in Computer Science",
+        "B.Sc. (Special) in Information Communication Technology",
+        "B.Sc. in Applied Mathematics and Computing",
+        "B.Sc. in Environmental Science",
+        "BSc (Special) Degree in mathematics with statistics",
         "Bachelor of Applied Science in Food Technology",
+        "Bachelor of Computer Science (General)",
+        "Bachelor of Computer Science (Special)",
+        "Bachelor of Information Technology (General)",
+        "Bachelor of Information Technology (Special)",
+        "Bachelor of Information and Communication Technology",
+        "Bachelor of Information and Communication Technology(General)",
+        "Bachelor of Science (Applied Science) in Environmental Science and Natural Resource Management",
+        "Bachelor of Science (Applied Sciences) Physical Sciences",
         "Bachelor of Science (General)",
+        "Bachelor of Science (General) in Applied Sciences",
         "Bachelor of Science (Special)",
+        "Bachelor of Science (Special) in Applied Biology",
+        "Bachelor of Science (Special) in Applied Physics",
+        "Bachelor of Science (Special) in Applied Sciences",
+        "Bachelor of Science (Special) in Applied Statistics",
         "Bachelor of Science (Special) in Biology",
+        "Bachelor of Science (Special) in Biology and Physics",
         "Bachelor of Science (Special) in Botany",
+        "Bachelor of Science (Special) in Chemical Technology",
         "Bachelor of Science (Special) in Chemistry",
-        "Bachelor of Science (Special) in Computer Science",
+        "Bachelor of Science (Special) in Computer Science and Technology",
+        "Bachelor of Science (Special) in Environmental Sciences and Natural Resources Management",
+        "Bachelor of Science (Special) in Food Science and Technology",
         "Bachelor of Science (Special) in Forestry and Environmental Science",
         "Bachelor of Science (Special) in Geology",
+        "Bachelor of Science (Special) in Health Promotion",
         "Bachelor of Science (Special) in Mathematics",
         "Bachelor of Science (Special) in Molecular Biology and Biotechnology",
         "Bachelor of Science (Special) in Physics",
         "Bachelor of Science (Special) in Plant Biotechnology",
-        "Bachelor of Science (Special) in Statistics"
+        "Bachelor of Science (Special) in Statistics",
+        "Bachelor of Science in (Joint Major)",
+        "Bachelor of Science in Applied Sciences (Physical)",
+        "Bachelor of Science in Computing and Information Systems",
+        "Bachelor of Science in Industrial Mathematics",
+        "Bachelor of Science in Physical Education",
+        "Bachelor of Science in Sports Sciences and Management"
       ],
-      "Faculty of Humanities & Social Sciences": [ 
+      "Faculty of Humanities & Social Sciences": [
+        "BA in English Language Teaching (BA/ELT)",
         "Bachelor of Arts (General)",
+        "Bachelor of Arts (Special)",
         "Bachelor of Arts (Special) in Anthropology",
         "Bachelor of Arts (Special) in Archaeology",
         "Bachelor of Arts (Special) in Buddhist Civilization",
+        "Bachelor of Arts (Special) in Buddhist Culture",
         "Bachelor of Arts (Special) in Buddhist Philosophy",
         "Bachelor of Arts (Special) in Criminology",
         "Bachelor of Arts (Special) in Cultural and Aesthetic Studies",
-        "Bachelor of Arts (Special) in Economics", 
+        "Bachelor of Arts (Special) in Economics",
         "Bachelor of Arts (Special) in English",
         "Bachelor of Arts (Special) in French",
         "Bachelor of Arts (Special) in Geography",
@@ -230,26 +245,36 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Arts (Special) in Management Information Technology",
         "Bachelor of Arts (Special) in Mass Communication",
         "Bachelor of Arts (Special) in Pali",
+        "Bachelor of Arts (Special) in Pali and Buddhist",
         "Bachelor of Arts (Special) in Philosophy and Psychology",
         "Bachelor of Arts (Special) in Political Science",
         "Bachelor of Arts (Special) in Sanskrit",
         "Bachelor of Arts (Special) in Sinhala",
         "Bachelor of Arts (Special) in Social Statistics",
-        "Bachelor of Arts (Special) in Sociology"
+        "Bachelor of Arts (Special) in Sociology",
+        "Bachelor of Arts in Social Sciences (BA)",
+        "Bachelor of Laws (LL.B)",
+        "Bachelor of Management Studies (BMS)"
       ],
       "Faculty of Management Studies and Commerce": [
+        "Bachelor of Business Administration (Accounting Special) (Special)",
+        "Bachelor of Business Administration (Financial Management Special) (Special)",
+        "Bachelor of Business Administration (Human Resource Management Special) (Special)",
+        "Bachelor of Business Administration (Marketing Special) (Special)",
+        "Bachelor of Business Management (Online)",
+        "Bachelor of Commerce",
         "Bachelor of Commerce (Special)",
         "Bachelor of Science (Special) in Accounting",
+        "Bachelor of Science (Special) in Business Administration (Business Economics)",
         "Bachelor of Science (Special) in Business Administrations",
-        "Bachelor of Science (Special) in Business Administration (Business Economics)", 
-        "Bachelor of Science in Business Administration (HRM) (Special)",
         "Bachelor of Science (Special) in Business Information Systems",
         "Bachelor of Science (Special) in Estate Management and Valuation",
         "Bachelor of Science (Special) in Finance",
         "Bachelor of Science (Special) in Human Entrepreneurship",
         "Bachelor of Science (Special) in Marketing Management",
         "Bachelor of Science (Special) in Operations and Technology Management",
-        "Bachelor of Science (Special) in Public Management"
+        "Bachelor of Science (Special) in Public Management",
+        "Bachelor of Science in Business Administration (HRM) (Special)"
       ],
       "Faculty of Medical Sciences": [
         "Bachelor of Pharmacy",
@@ -317,14 +342,12 @@ export const legacyData: LegacyUniversityData = {
       "Faculty of Architecture": [
         "Bachelor of Landscape Architecture",
         "Honours Degree of Bachelor of Architecture",
-        "Honours Degree of Bachelor of Architecture",
         "Honours Degree of Bachelor of Science in Facilities Management",
         "Honours Degree of Bachelor of Science in Quantity Surveying",
         "Honours Degree of Bachelor of Science in Town and Country Planning"
       ],
       "Faculty of Information Technology": [
         "Degree of Bachelor of Information Technology - External Degree",
-        "Degree of Bachelor of Science in Information Technology",
         "Degree of Bachelor of Science in Information Technology",
         "Honours Degree of Bachelor of Science in Information Technology and Management"
       ]
@@ -382,7 +405,7 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Arts (Special) in Archaeology",
         "Bachelor of Arts (Special) in Buddhist Culture",
         "Bachelor of Arts (Special) in Buddhist Philosophy",
-        "Bachelor of Arts (Special) in Economics", 
+        "Bachelor of Arts (Special) in Economics",
         "Bachelor of Arts (Special) in Geography",
         "Bachelor of Arts (Special) in History",
         "Bachelor of Arts (Special) in Pali and Buddhist",
@@ -436,23 +459,17 @@ export const legacyData: LegacyUniversityData = {
       "Faculty of Arts and Culture": [
         "Bachelor of Arts (General)",
         "Bachelor of Arts (Special)",
-        "Bachelor of Arts (Special) in Economics", 
+        "Bachelor of Arts (Special) in Economics",
         "Bachelor of Arts (Special) in Hindu Civilization",
         "Bachelor of Arts (Special) in Philosophy",
         "Bachelor of Arts (Special) in Political Science",
         "Bachelor of Arts (Special) in Sociology",
         "Bachelor of Arts (Special) in Tamil",
-        "Degree of Bachelor of Arts (General)",
         "Degree of Bachelor of Arts Special in Drama and Theatre",
-        "Degree of Bachelor of Arts Special in Economics",
         "Degree of Bachelor of Arts Special in Education",
         "Degree of Bachelor of Arts Special in Fine Arts",
         "Degree of Bachelor of Arts Special in Geography",
-        "Degree of Bachelor of Arts Special in Hindu Culture",
-        "Degree of Bachelor of Arts Special in Philosophy",
-        "Degree of Bachelor of Arts Special in Political Science",
-        "Degree of Bachelor of Arts Special in Sociology",
-        "Degree of Bachelor of Arts Special in Tamill"
+        "Degree of Bachelor of Arts Special in Hindu Culture"
       ],
       "Faculty of Commerce and Management": [
         "Degree of Bachelor of Business Administration Specialization in Human Resource Management",
@@ -461,7 +478,6 @@ export const legacyData: LegacyUniversityData = {
         "Degree of Bachelor of Commerce Specialization in Enterprise Development"
       ],
       "Faculty of Science": [
-        "Bachelor of Science (General)",
         "Degree of Bachelor of Science Special in Botany",
         "Degree of Bachelor of Science Special in Chemistry",
         "Degree of Bachelor of Science Special in Computer Science and Mathematics",
@@ -538,7 +554,7 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Arts (General)",
         "Bachelor of Arts (Special)",
         "Bachelor of Arts (Special) in Archaeology and Heritage Management",
-        "Bachelor of Arts (Special) in Economics", 
+        "Bachelor of Arts (Special) in Economics",
         "Bachelor of Arts (Special) in Environmental Management",
         "Bachelor of Arts (Special) in History",
         "Bachelor of Arts (Special) in Sinhala"
@@ -594,7 +610,7 @@ export const legacyData: LegacyUniversityData = {
         "Bachelor of Science (Special) Tourism Management"
       ],
       "Faculty of Social Sciences & Languages": [
-        "Bachelor of Arts (Special) Economics", 
+        "Bachelor of Arts (Special) Economics",
         "Bachelor of Arts (Special) Geography",
         "Bachelor of Arts (Special) Political Science",
         "Bachelor of Arts (Special) Sociology and Anthropology",
@@ -681,7 +697,7 @@ export const legacyData: LegacyUniversityData = {
       "Faculty of Business Studies": [
         "Bachelor of Business Management (General)",
         "Bachelor of Business Management (Special) in Accountancy and Finance",
-        "Bachelor of Business Management (Special) in Business Economics", 
+        "Bachelor of Business Management (Special) in Business Economics",
         "Bachelor of Business Management (Special) in Marketing Management"
       ]
     },
